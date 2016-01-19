1182988bdae3ff30bdaf563c52406e9230c9696d
Ron Fields <eightbits72@gmail.com>
2016-01-19 16:02:10 -0600

    Initial commit.

---
+++ Test.DapperORMWithMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test.DapperORMWithMVC.Models;
+using Test.DapperORMWithMVC.Repository;
+
+namespace Test.DapperORMWithMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee/GetAllEmpDetails      
+        public ActionResult GetAllEmpDetails()
+        {
+            EmpRepository EmpRepo = new EmpRepository();
+            return View(EmpRepo.GetAllEmployees());
+        }
+
+        // GET: Employee/AddEmployee      
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        // POST: Employee/AddEmployee      
+        [HttpPost]
+        public ActionResult AddEmployee(EmpModel Emp)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    EmpRepository EmpRepo = new EmpRepository();
+                    EmpRepo.AddEmployee(Emp);
+
+                    ViewBag.Message = "Records added successfully.";
+
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Bind controls to Update details      
+        public ActionResult EditEmpDetails(int id)
+        {
+            EmpRepository EmpRepo = new EmpRepository();
+            return View(EmpRepo.GetAllEmployees().Find(Emp => Emp.Empid == id));
+
+        }
+
+        // POST:Update the details into database      
+        [HttpPost]
+        public ActionResult EditEmpDetails(int id, EmpModel obj)
+        {
+            try
+            {
+                EmpRepository EmpRepo = new EmpRepository();
+
+                EmpRepo.UpdateEmployee(obj);
+
+                return RedirectToAction("GetAllEmpDetails");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Delete  Employee details by id      
+        public ActionResult DeleteEmp(int id)
+        {
+            try
+            {
+                EmpRepository EmpRepo = new EmpRepository();
+                if (EmpRepo.DeleteEmployee(id))
+                {
+                    ViewBag.AlertMsg = "Employee details deleted successfully";
+
+                }
+                return RedirectToAction("GetAllEmpDetails");
+            }
+            catch
+            {
+                return RedirectToAction("GetAllEmpDetails");
+            }
+        }
+    }
+}
---
+++ Test.DapperORMWithMVC/Repository/EmpRepository.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+using Test.DapperORMWithMVC.Models;
+
+namespace Test.DapperORMWithMVC.Repository
+{
+    public class EmpRepository
+    {
+        public SqlConnection con;
+        // connection activities      
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["SqlConn"].ToString();
+            con = new SqlConnection(constr);
+
+        }
+
+        // Add Employee
+        public void AddEmployee(EmpModel objEmp)
+        {
+
+            //Additing the employess      
+            try
+            {
+                connection();
+                con.Open();
+                con.Execute("AddNewEmpDetails", objEmp, commandType: CommandType.StoredProcedure);
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        // get employees
+        public List<EmpModel> GetAllEmployees()
+        {
+            try
+            {
+                connection();
+                con.Open();
+                IList<EmpModel> EmpList = SqlMapper.Query<EmpModel>(
+                                  con, "GetEmployees").ToList();
+                con.Close();
+                return EmpList.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        // update Employee
+        public void UpdateEmployee(EmpModel objUpdate)
+        {
+            try
+            {
+                connection();
+                con.Open();
+                con.Execute("UpdateEmpDetails", objUpdate, commandType: CommandType.StoredProcedure);
+                con.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        // delete Employee
+        public bool DeleteEmployee(int Id)
+        {
+            try
+            {
+                DynamicParameters param = new DynamicParameters();
+                param.Add("@EmpId", Id);
+                connection();
+                con.Open();
+                con.Execute("DeleteEmpById", param, commandType: CommandType.StoredProcedure);
+                con.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log errors as needed       
+                throw ex;
+            }
+        }
+    }
+}
